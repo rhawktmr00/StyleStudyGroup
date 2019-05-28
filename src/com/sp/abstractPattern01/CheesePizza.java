8bcab544d0e0d6c1230ffc03f5031d55d7a70dc1
chou9827 <chou9827@gmail.com>
2019-05-28 12:27:28 +0900

    question abstract Pattern

--- src/com/sp/abstractPattern01/CheesePizza.java
+++ src/com/sp/abstractPattern01/CheesePizza.java
@@ -1,18 +1,6 @@
 package com.sp.abstractPattern01;
 
-import com.sp.common.systemOutPrint;
+public class CheesePizza {
 
-public class CheesePizza extends Pizza{
-
-
-  @Override
-  protected void topping() {
-      // TODO Auto-generated method stub
-      new systemOutPrint("topping Cheese...");
-  }
-  @Override
-  public String pizzaName() {
-      return "Cheese Pizza";
-  }
 
 }
--- src/com/sp/abstractPattern01/PizzaStore.java
+++ src/com/sp/abstractPattern01/PizzaStore.java
@@ -2,34 +2,21 @@ package com.sp.abstractPattern01;
 
 public class PizzaStore {
 
-  
-  Pizza pizza;
-  PizzaMenuFactory factory;
-  
-  public PizzaStore() {
-      this.factory = new PizzaMenuFactory();
-  }
-  
-  public Pizza orderPizza(String type){
-      //menu펙토리에서 주문한 피자를 만듬
-      pizza = factory.createPizza(type);
+
+
+  public void orderPizza(String type) {
+
+      // menu
+      if (type.equals("cheese")) {
       
-      makePizza();
+      } else if (type.equals("pepperoni")) {
       
-      return pizza;
-  }
+      } else if (type.equals("chicken")) {
+      
+      } else if (type.equals("veggie")) {
   
-  private void makePizza(){
-      //준비하기
-      pizza.prepare();
-      //토핑하기
-      pizza.topping(); // 메뉴팩토리를 통해 관리
-      //굽기
-      pizza.bake();
-      //자르기
-      pizza.cut();
-      //박스에 넣기
-      pizza.box();
+      }
+
   }
-  
+
 }
