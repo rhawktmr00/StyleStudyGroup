6ee87f3d479237c4884ea8903aa23761e137d2a2
chou9827 <chou9827@gmail.com>
2019-05-28 12:16:04 +0900

    question decoratePattern

--- src/com/sp/decoratePattern01/Soy.java
+++ src/com/sp/decoratePattern01/Soy.java
@@ -1,24 +1,5 @@
 package com.sp.decoratePattern01;
 
-public class Soy extends CondimentDecorator {
-
-  Beverage beverage;
-  
-  public Soy(Beverage beverage) {
-    // TODO Auto-generated constructor stub
-    this.beverage = beverage;
-  }
-  
-  @Override
-  public String getDescription() {
-    // TODO Auto-generated method stub
-    return beverage.getDescription() + "Soy ";
-  }
-
-  @Override
-  public double cost() {
-    // TODO Auto-generated method stub
-    return 0.3;
-  }
+public class Soy {
 
 }
